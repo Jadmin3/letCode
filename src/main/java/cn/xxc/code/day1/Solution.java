bc71240eca64632ed359018167194e18fb4d2bf7
Jadmin <1243678726@qq.com>
2020-04-20 00:21:38 +0800

    给定一个数组和一个目标值 返回他们的数组下标

--- src/main/java/cn/xxc/code/day1/Solution.java
+++ src/main/java/cn/xxc/code/day1/Solution.java
@@ -1,6 +1,9 @@
 package cn.xxc.code.day1;
 
 
+
+import java.util.HashMap;
+
 /**
  * @author Jadmin
  * @Date 2020-04-19
@@ -24,7 +27,28 @@ public class Solution {
      * 链接：https://leetcode-cn.com/problems/two-sum
      * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
      */
-    public static void main(String[] args) {
-
+    private static int[] sum(int[] nums, int target){
+        int[] indexs = new int[2];
+        HashMap<Integer, Integer> hashMap = new HashMap<Integer, Integer>(16);
+        for (int i = 0; i < nums.length; i++) {
+            //下次在遍历如果包含 就把这个数的下标放在map里 则结束
+            if (hashMap.containsKey(nums[i])) {
+                indexs[0] = i;
+                indexs[1] = hashMap.get(nums[i]);
+                return indexs;
+            }
+            //目标值减去下标位 并存到map
+            hashMap.put(target - nums[i], i);
+        }
+        return indexs;
     }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{2, 7, 11, 15};
+        int[] sum = sum(nums, 13);
+        for (int i = 0; i < sum.length; i++) {
+            System.out.println(sum[i]);
+        }
+    }
+
 }
