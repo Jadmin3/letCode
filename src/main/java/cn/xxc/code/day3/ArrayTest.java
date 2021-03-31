99733f35f45c7d999977744281c856b810b8805d
Jadmin <admin@xxc520.cn>
2021-03-31 21:15:18 +0800

    feat:给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引

--- src/main/java/cn/xxc/code/day3/ArrayTest.java
+++ src/main/java/cn/xxc/code/day3/ArrayTest.java
@@ -1,14 +1,13 @@
 package cn.xxc.code.day3;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class ArrayTest {
 
 
     public static void main(String[] args) {
+        int [] nums=  new int[]{1,3,5,6};
+        System.out.println(searchInsert(nums, 7));
     }
 
     /**
@@ -37,7 +36,6 @@ public class ArrayTest {
 
     public int[] getIntersection(Set<Integer> set1, Set<Integer> set2) {
         //首先根据题解知道是找交集  那就是集合更小的去集合更大的地方一个一个对比  如果符合就把这个数找出来
-
         //这里是如果结果相反则集合对调
         if (set1.size() > set2.size()) {
             return getIntersection(set2, set1);
@@ -58,5 +56,59 @@ public class ArrayTest {
         }
         return result;
     }
+
+    public int[] intersection2(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0, j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] == nums2[j]) {
+                set.add(nums1[i]);
+                i++;
+                j++;
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            }
+        }
+        int[] res = new int[set.size()];
+        int index = 0;
+        for (int num : set) {
+            res[index++] = num;
+        }
+        return res;
+    }
+
+    /**
+     *
+     * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+     *
+     * 你可以假设数组中无重复元素。
+     *
+     * 示例 1:
+     *
+     * 输入: [1,3,5,6], 5
+     * 输出: 2
+     *
+     * 作者：力扣 (LeetCode)
+     * 链接：https://leetcode-cn.com/leetbook/read/array-and-string/cxqdh/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int searchInsert(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            //如果等于直接返回下标 不然直接返回数组长度 因为下标从0开始 数组长度不是 默认加1就是目标的下标
+            if (nums[i] >= target) {
+                return i;
+            }
+        }
+        return nums.length;
+    }
+
 
 }
